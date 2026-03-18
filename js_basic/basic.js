137de4b4aeb8409d1315f8b5284fe81178acbb74
Ashish kumar jha <ashishjha3883@gmail.com>
2026-03-18 18:03:14 +0000

    basic code updated

---
+++ js_basic/basic.js
@@ -0,0 +1,13 @@
+var accountId = 938403
+let accountHolder = "One Ash"
+const MobileNo = 923
+salary = 42000000
+occupation_isValid = true
+console.log(typeof "you are mine");
+console.log(typeof salary);
+console.table([accountHolder,accountId,MobileNo,occupation_isValid])
+
+
+
+
+
